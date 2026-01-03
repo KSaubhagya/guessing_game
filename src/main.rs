377e635865007e469368df7c7f68622251420a07
wein1m <weynn.sz@gmail.com>
2026-01-03 18:53:10 +0800

    add loop and handle non-int value

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,33 @@ fn main() {
     println!("-------- GUESSING GAME!!! !-------");
 
     let answer: u32 = rand::thread_rng().gen_range(1..=10);
-    let mut guess = String::new();
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line :(");
-    
-    let guess: u32  = guess.trim().parse()
-        .expect("number only, pwease!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line :(");
+        
+        let guess: u32  = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Number only, pwease!");
+                continue;
+            },
+        };
 
-    println!("The answer isss... {answer}");
-    println!("Your guess: {guess}");
+        println!("The answer isss... {answer}");
+        println!("Your guess: {guess}");
 
-    match guess.cmp(&answer) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Equal => println!("You Win!"),
-        Ordering::Greater => println!("Too Big!"),
+        match guess.cmp(&answer) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal =>  {
+                println!("You Win!");
+                break;
+            },
+        }
     }
 }
